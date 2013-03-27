544abab8e50f71ee6f3bc723d316b9b1d4bcd96d
Jarrett Cruger <jcruger@qrhook.com>
2013-03-27 09:36:35 -0400

    Added an else clause for route responses

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  res.setHeader('content-type', 'text/plain');
   if (req.url === '/') {
-    res.setHeader('content-type', 'text/plain');
     res.end('hithere');
+  }
+  else {
+    res.end('fackkkkkk you');
   }
 }).listen(3000);
