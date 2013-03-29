7dfcedf43140519d4bc1636557c4ef376c81b279
Jarrett Cruger <jcruger@qrhook.com>
2013-03-29 12:07:15 -0400

    added a route and ecstatic

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require('http');
+var ecstatic = require('ecstatic');
 
 http.createServer(function (req, res) {
   res.setHeader('content-type', 'text/plain');
   if (req.url === '/') {
     res.end('hithere');
   }
-  else {
+  else if (req.url === '/test') {
     res.end('fackkkkkk you');
   }
+  else ecstatic(req, res);
+
 }).listen(3000);
